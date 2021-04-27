70b2b547099b90ece9d3a0f87e44df349c542b64
Alejandra Vega <alevega1997@gmail.com>
2021-04-26 19:01:20 -0500

    search js

--- style.js
+++ style.js
@@ -7,32 +7,22 @@ let day = days[now.getDay()];
 h1.innerHTML = day + " " + hours + ":" + minutes;
 
 function showWeather(response) {
-  event.preventDefault();
   let h2 = document.querySelector("h2");
-  let temperature = Math.rouns(response.data.main.temp);
-  h2.innerHTML = ["In" + city + " the weather is" + temperature + "°C"];
+  let city = response.data.name;
+  let temperature = Math.round(response.data.main.temp);
+  h2.innerHTML = "In " + city + " the weather is " + temperature + " °C";
 }
-
-function search(event) {
-  event.preventDefault();
-  let cityElement = document.querySelector("#city");
-  let cityInput = document.querySelector("#city-input");
-  cityElement.innerHTML = cityInput.value;
-}
-
 function searchCity(city) {
-  let apiKey = "8bb604049c6fff46ea57c55ff62d89d7";
+  let apiKey = "146f60b835b0f2ca6da53fce0c0bb62f";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayWeatherCondition);
+  axios.get(apiUrl).then(showWeather);
 }
-
 function handleSubmit(event) {
   event.preventDefault();
-  let city = document.querySelector("#password-form");
-  searchCity(city);
-  let cityInput = document.querySelector("#password-input");
-  city.innerHTML = cityInput.value;
+  let h2 = document.querySelector("h2");
+  let cityInput = document.querySelector("#city-input");
+  h2.innerHTML = cityInput.value;
+  searchCity(cityInput.value);
 }
-
-let searchForm = document.querySelector("#password-input");
+let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmit);
